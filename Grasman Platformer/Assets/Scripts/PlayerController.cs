05a11a242e0fd8e01b14c59714b69cfc0e2216b0
Sophie_G <rosaulula@gmail.com>
2024-11-13 21:26:09 -0500

    Left and right movement

--- Grasman Platformer/Assets/Scripts/PlayerController.cs
+++ Grasman Platformer/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public Rigidbody2D playerRigidbody;
+    public float acceleration;
+    public float maxSpeed = 5f;
     public enum FacingDirection
     {
         left, right
@@ -21,12 +25,62 @@ public class PlayerController : MonoBehaviour
         //The input from the player needs to be determined and then passed in the to the MovementUpdate which should
         //manage the actual movement of the character.
         Vector2 playerInput = new Vector2();
+        Vector2 previousInput = new Vector2();
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            playerInput = Vector2.left;
+            previousInput = Vector2.left;
+
+            acceleration += 0.01f;
+
+            if (acceleration >= maxSpeed)
+            {
+                acceleration = maxSpeed;
+            }
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            playerInput = Vector2.right;
+            previousInput = Vector2.right;
+
+            acceleration += 0.01f;
+
+            if (acceleration >= maxSpeed)
+            {
+                acceleration = maxSpeed;
+            }
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            playerInput = Vector2.up;
+        }
+        else
+        {
+            acceleration -= 0.01f;
+            if (acceleration <= 0)
+            {
+                acceleration = 0;
+            }
+        }
+
+        if (acceleration > 0)
+        {
+            playerInput = previousInput;
+        }
+        else if (acceleration == 0)
+        {
+            playerInput = Vector2.zero;
+        }
+        
         MovementUpdate(playerInput);
     }
 
     private void MovementUpdate(Vector2 playerInput)
     {
 
+        playerRigidbody.velocity = acceleration * playerInput;
+ 
     }
 
     public bool IsWalking()
