8027dc49444bfd7f804a54cd258d6633a993a128
Sophie_G <rosaulula@gmail.com>
2024-11-14 17:16:33 -0500

    Movement complete
    well mostly complete :/

--- Grasman Platformer/Assets/Scripts/PlayerController.cs
+++ Grasman Platformer/Assets/Scripts/PlayerController.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class PlayerController : MonoBehaviour
 {
 
     public Rigidbody2D playerRigidbody;
     public float acceleration;
-    public float maxSpeed = 5f;
+    public float maxSpeed = 200f;
+    public float accelSpeed = 10f;
     public enum FacingDirection
     {
         left, right
@@ -20,19 +22,17 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         //The input from the player needs to be determined and then passed in the to the MovementUpdate which should
         //manage the actual movement of the character.
         Vector2 playerInput = new Vector2();
-        Vector2 previousInput = new Vector2();
 
         if (Input.GetKey(KeyCode.A))
         {
             playerInput = Vector2.left;
-            previousInput = Vector2.left;
 
-            acceleration += 0.01f;
+            acceleration += accelSpeed;
 
             if (acceleration >= maxSpeed)
             {
@@ -42,9 +42,8 @@ public class PlayerController : MonoBehaviour
         else if (Input.GetKey(KeyCode.D))
         {
             playerInput = Vector2.right;
-            previousInput = Vector2.right;
 
-            acceleration += 0.01f;
+            acceleration += accelSpeed;
 
             if (acceleration >= maxSpeed)
             {
@@ -54,33 +53,34 @@ public class PlayerController : MonoBehaviour
         else if (Input.GetKey(KeyCode.W))
         {
             playerInput = Vector2.up;
+            Debug.Log("jumped!");
         }
         else
         {
-            acceleration -= 0.01f;
-            if (acceleration <= 0)
-            {
-                acceleration = 0;
-            }
+            acceleration = 0;
         }
 
-        if (acceleration > 0)
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
         {
-            playerInput = previousInput;
-        }
-        else if (acceleration == 0)
-        {
-            playerInput = Vector2.zero;
+            acceleration = 0;
         }
         
         MovementUpdate(playerInput);
+
     }
 
     private void MovementUpdate(Vector2 playerInput)
     {
+        if (playerInput == Vector2.left || playerInput == Vector2.right)
+        {
+            playerRigidbody.velocity = acceleration * playerInput * Time.deltaTime;
+        }
+        else if (playerInput == Vector2.up)
+        {
+            playerRigidbody.velocity = 450 * Vector2.up * Time.deltaTime;
+        }
 
-        playerRigidbody.velocity = acceleration * playerInput;
- 
+        //playerRigidbody.velocity = acceleration * playerInput * Time.deltaTime;
     }
 
     public bool IsWalking()
